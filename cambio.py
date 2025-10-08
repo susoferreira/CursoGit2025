c1ba8ab3bdaef1f4c08b8ace52679a76ef8dc281
susoferreira <ferreirarodriguezjesus@gmail.com>
2025-10-08 18:48:53 +0200

    codigo

--- cambio.py
+++ cambio.py
@@ -4,3 +4,29 @@ def add(a,b):
 
 def sub(a,b):
     return a - b
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Hello Sally"
+
+print(myfunction())
